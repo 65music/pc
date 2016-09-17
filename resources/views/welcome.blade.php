e0f98c3449ec56f3657b0be8cd85cf75e89c31da
knight322 <zhjg6018@163.com>
2016-09-17 14:34:22 +0800

    player added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,155 @@
+<html>
+    <header class="am-topbar am-topbar-inverse">
+    	<!-- ZUI 标准版压缩后的 CSS 文件 -->
+		<link rel="stylesheet" href="//cdn.bootcss.com/zui/1.5.0/css/zui.min.css">
+    </header>
+    <body>
+    	<nav class="navbar navbar-inverse text-center" role="navigation">
+    		<span><i class="icon icon-music icon-2x"></i></span>
+    	</nav>
+        <div class="container">
+        	<div class="items">
+				<div class="item">
+					<div class="panel">
+  						<div class="panel-body">
+					    	<div class="item-heading">
+					      		<div class="pull-right label label-success">在库</div>
+					      		<h4></h4>
+					    	</div>
+					    	<div class="item-content media">
+					    		<div id="music_1"></div>
+					    		<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+					    			<input type="image" src="http://zui.sexy/docs/img/img2.jpg" role="button" width="150px" height="150px" class="jp-play pull-left img-circle" />
+					    		</div>
+					    		<div class="text" style="margin-left: 250px;">
+					    			<span style="font-size: 15px;"><i class="icon icon-headphones"></i></span>&nbsp;壁虎漫步<br/><br/>
+					    			<span>歌手：</span><span>潘玮柏</span><br/><br/>
+					    			<span style="font-size: 15px;">潘帅的Tell me,有点10年前的old school style</span>
+					    		</div>
+					    	</div>
+					    	<div class="item-footer pull-right">
+					      		<a href="#" class="text-muted"><i class="icon-comments"></i> 243</a> &nbsp; <span class="text-muted">2013-11-11 16:14:37</span>
+					    	</div>
+					    </div>
+					</div>
+			  	</div>
+			  	<div class="item">
+					<div class="panel">
+  						<div class="panel-body">
+					    	<div class="item-heading">
+					      		<div class="pull-right label label-success">在库</div>
+					      		<h4></h4>
+					    	</div>
+					    	<div class="item-content media">
+					    		<div id="music_1"></div>
+					    		<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+					    			<input type="image" src="http://zui.sexy/docs/img/img2.jpg" role="button" width="150px" height="150px" class="jp-play pull-left img-circle" />
+					    		</div>
+					    		<div class="text" style="margin-left: 250px;">
+					    			<span style="font-size: 15px;"><i class="icon icon-headphones"></i></span>&nbsp;壁虎漫步<br/><br/>
+					    			<span>歌手：</span><span>潘玮柏</span><br/><br/>
+					    			<span style="font-size: 15px;">潘帅的Tell me,有点10年前的old school style</span>
+					    		</div>
+					    	</div>
+					    	<div class="item-footer pull-right">
+					      		<a href="#" class="text-muted"><i class="icon-comments"></i> 243</a> &nbsp; <span class="text-muted">2013-11-11 16:14:37</span>
+					    	</div>
+					    </div>
+					</div>
+			  	</div>
+			  	<div class="item">
+					<div class="panel">
+  						<div class="panel-body">
+					    	<div class="item-heading">
+					      		<div class="pull-right label label-success">在库</div>
+					      		<h4></h4>
+					    	</div>
+					    	<div class="item-content media">
+					    		<div id="music_1"></div>
+					    		<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+					    			<input type="image" src="http://zui.sexy/docs/img/img2.jpg" role="button" width="150px" height="150px" class="jp-play pull-left img-circle" />
+					    		</div>
+					    		<div class="text" style="margin-left: 250px;">
+					    			<span style="font-size: 15px;"><i class="icon icon-headphones"></i></span>&nbsp;壁虎漫步<br/><br/>
+					    			<span>歌手：</span><span>潘玮柏</span><br/><br/>
+					    			<span style="font-size: 15px;">潘帅的Tell me,有点10年前的old school style</span>
+					    		</div>
+					    	</div>
+					    	<div class="item-footer pull-right">
+					      		<a href="#" class="text-muted"><i class="icon-comments"></i> 243</a> &nbsp; <span class="text-muted">2013-11-11 16:14:37</span>
+					    	</div>
+					    </div>
+					</div>
+			  	</div>
+			  	<div class="item">
+					<div class="panel">
+  						<div class="panel-body">
+					    	<div class="item-heading">
+					      		<div class="pull-right label label-success">在库</div>
+					      		<h4></h4>
+					    	</div>
+					    	<div class="item-content media">
+					    		<div id="music_1"></div>
+					    		<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+					    			<input type="image" src="http://zui.sexy/docs/img/img2.jpg" role="button" width="150px" height="150px" class="jp-play pull-left img-circle" />
+					    		</div>
+					    		<div class="text" style="margin-left: 250px;">
+					    			<span style="font-size: 15px;"><i class="icon icon-headphones"></i></span>&nbsp;壁虎漫步<br/><br/>
+					    			<span>歌手：</span><span>潘玮柏</span><br/><br/>
+					    			<span style="font-size: 15px;">潘帅的Tell me,有点10年前的old school style</span>
+					    		</div>
+					    	</div>
+					    	<div class="item-footer pull-right">
+					      		<a href="#" class="text-muted"><i class="icon-comments"></i> 243</a> &nbsp; <span class="text-muted">2013-11-11 16:14:37</span>
+					    	</div>
+					    </div>
+					</div>
+			  	</div>
+			  	<div class="item">
+					<div class="panel">
+  						<div class="panel-body">
+					    	<div class="item-heading">
+					      		<div class="pull-right label label-success">在库</div>
+					      		<h4></h4>
+					    	</div>
+					    	<div class="item-content media">
+					    		<div id="music_1"></div>
+					    		<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+					    			<input type="image" src="http://zui.sexy/docs/img/img2.jpg" role="button" width="150px" height="150px" class="jp-play pull-left img-circle" />
+					    		</div>
+					    		<div class="text" style="margin-left: 250px;">
+					    			<span style="font-size: 15px;"><i class="icon icon-headphones"></i></span>&nbsp;壁虎漫步<br/><br/>
+					    			<span>歌手：</span><span>潘玮柏</span><br/><br/>
+					    			<span style="font-size: 15px;">潘帅的Tell me,有点10年前的old school style</span>
+					    		</div>
+					    	</div>
+					    	<div class="item-footer pull-right">
+					      		<a href="#" class="text-muted"><i class="icon-comments"></i> 243</a> &nbsp; <span class="text-muted">2013-11-11 16:14:37</span>
+					    	</div>
+					    </div>
+					</div>
+			  	</div>
+			</div>
+        </div>
+        <!-- ZUI Javascript 依赖 jQuery -->
+		<script src="//cdn.bootcss.com/zui/1.5.0/lib/jquery/jquery.js"></script>
+		<!-- ZUI 标准版压缩后的 JavaScript 文件 -->
+		<script src="//cdn.bootcss.com/zui/1.5.0/js/zui.min.js"></script>
+		<script src="/plugin/jPlayer-2.9.1/dist/jplayer/jquery.jplayer.min.js"></script>
+		<script type="text/javascript">
+			//<![CDATA[
+			$(document).ready(function(){
+				$("#music_1").jPlayer({
+					ready: function (event) {
+						$(this).jPlayer("setMedia", {
+							mp3: "/knight/2016/09/17/今生共相伴.mp3"
+						});
+					},
+					supplied: "mp3, m4a, oga",
+					useStateClassSkin: true
+				});
+			});
+			//]]>
+		</script>
+    </body>
+</html>
